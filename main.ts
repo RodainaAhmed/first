8847a0eba5aed27ba49daa8bd04436def1702055
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-06-12 14:23:52 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Breve))
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showString("rody")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showString("rovy")
+    }
+})
