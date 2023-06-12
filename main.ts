2ee93c3605c00d1f1a8b25ba8ad85925d835f037
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-06-12 14:27:59 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(262, music.beat(BeatFraction.Breve))
     for (let index = 0; index < 2; index++) {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
         basic.showString("rody")
     }
 })
